d3296685217d5b1b810df179d20e0d8ef31bbe10
Michelle Jauge <asesinevil@gmail.com>
2024-06-22 10:27:14 -0300

    new order

--- data_access/db_connection.js
+++ data_access/db_connection.js
@@ -1,1 +1,11 @@
-const connection = require('mysql');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: process.config.DB_HOST,
+    port: 3036,
+    user: process.config.DB_USER,
+    password: process.config.DB_PASS,
+    database: process.config.DB_DATABASE,
+});
+
+module.exports = connection;
